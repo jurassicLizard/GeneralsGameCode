ebc7fad69a5616c5a796579e696b3e825a449d8a
Salem B. <jurassic.lizard@protonmail.com>
2026-01-20 14:08:19 +0100

    Fix copyright header and space issues

--- Core/Libraries/Source/ImGui/dx8_backend/imgui_impl_dx8.cpp
+++ Core/Libraries/Source/ImGui/dx8_backend/imgui_impl_dx8.cpp
@@ -1,10 +1,43 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2025 Omar Cornut (Based on dx9 ImGui Backend)
+ * Copyright (c) 2021 KoMaR1911
+ * Copyright (c) 2025 Meigyoku-Thmn
+ * Copyright (c) 2026 TheSuperHackers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file imgui_impl_dx8.cpp
+ * @brief DirectX8 renderer backend for Dear ImGui, reverse-engineered from the official DX9 backend
+ *
+ * This needs to be used along with a Platform Backend (e.g. Win32)
+ */
+
 #include <imgui.h>
 #include "imgui_impl_dx8.h"
 
 // DirectX
 #include "d3d8.h"
 
-// based on https://github.com/KoMaR1911/C4USMultiHack-Metin2/blob/main/EngineX-Pro/ImGui/imgui_impl_dx8.cpp
 
 // DirectX data
 struct ImGui_ImplDX8_Data {
