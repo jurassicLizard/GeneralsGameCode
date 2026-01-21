34a4dc6a7302e9485d85b11b236633b18b4c3ba3
Salem B. <jurassic.lizard@protonmail.com>
2026-01-21 17:31:08 +0100

    Add copyright headers and fix indentation in frame mgr

--- Core/Libraries/Source/ImGui/wrapper/ImGuiFrameManager.cpp
+++ Core/Libraries/Source/ImGui/wrapper/ImGuiFrameManager.cpp
@@ -1,3 +1,24 @@
+/**
+ * @file ImGuiFrameManager.cpp
+ * @brief Simple Frame manager for ImGui widgets
+ */
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2026 TheSuperHackers
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "ImGuiFrameManager.h"
 #include "imgui.h"
 #include "imgui_impl_dx8.h"
@@ -7,25 +28,28 @@ bool ImGui::FrameManager::s_frameOpen = false;
 
 void ImGui::FrameManager::BeginFrame()
 {
-    if (s_frameOpen) {
+    if (s_frameOpen)
+    {
         return;
     }
 
     ImGui_ImplDX8_NewFrame();
     ImGui_ImplWin32_NewFrame();
-    ImGui::NewFrame();
+    NewFrame();
 
     s_frameOpen = true;
 }
 
 void ImGui::FrameManager::EndFrame()
 {
-    if (!s_frameOpen) {
+    if (!s_frameOpen)
+    {
         return;
     }
 
-    ImGui::Render();
+    Render();
 
     s_frameOpen = false;
 }
+
 
